2e51de63b3173c8d2893f501a72c4e3ea0a9131d
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-02-28 18:35:27 +0530

    ADDED:Winning condition

--- snakeAndLadderSimulation.sh
+++ snakeAndLadderSimulation.sh
@@ -13,28 +13,33 @@ echo "Welcome to Snake and Ladder game"
 # Roll a die to get ranmdom number between 1 to 6
 rollDieOutcome=$((RANDOM%6+1))
 echo "Roll die outcome is:" $rollDieOutcome
-
-optionCheck=$((RANDOM%3))
-case $optionCheck in
-	$noPlay)
-		echo "Current position is:"$position
-		echo "You stay in same position:"$position
-		;;
-	$ladder)
-		((ladderCount++))
-		echo "Current position is :" $position
-		position=$((position+rollDieOutcome))
-		echo "You encountered a ladder. new position is:" $position
-		;;
-	$snake)
-		((snakeCount++))
-		echo "Current position is :" $position
-		position=$((position-rollDieOutcome))
-		if(($position<0))
-		then
-			echo "You encountered a snake. new position is:" $POSITION
-		else
-			echo "You encountered a snake. new position is:" $position
-		fi
-		;;
-esac
+#Winning condition
+while(($position!=100))
+do
+	optionCheck=$((RANDOM%3))
+	case $optionCheck in
+		$noPlay)
+			echo "Current position is:"$position
+			echo "You stay in same position:"$position
+			;;
+		$ladder)
+			((ladderCount++))
+			echo "Current position is :" $position
+			position=$((position+rollDieOutcome))
+			echo "You encountered a ladder. new position is:" $position
+			;;
+		$snake)
+			((snakeCount++))
+			echo "Current position is :" $position
+			position=$((position-rollDieOutcome))
+			if(($position<0))
+			then
+				echo "You encountered a snake. new position is:" $POSITION
+				position=$POSITION
+			else
+				echo "You encountered a snake. new position is:" $position
+				position=$position
+			fi
+			;;
+	esac
+done
