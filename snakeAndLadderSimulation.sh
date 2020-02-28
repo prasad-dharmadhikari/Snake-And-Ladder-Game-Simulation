e6ef5bed71ed644b7628103d90ffcd00d197a1b0
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-02-28 19:01:42 +0530

    ADDED:Exact winning condition

--- snakeAndLadderSimulation.sh
+++ snakeAndLadderSimulation.sh
@@ -2,44 +2,55 @@
 
 #Constants
 POSITION=0
+WINNING_POSITION=100
 #Variables 
 position=0
 noPlay=0
 ladder=1
 snake=2
-ladderCount=0
-snakeCount=0
 echo "Welcome to Snake and Ladder game"
 # Roll a die to get ranmdom number between 1 to 6
-rollDieOutcome=$((RANDOM%6+1))
-echo "Roll die outcome is:" $rollDieOutcome
 #Winning condition
-while(($position!=100))
+while(($position!=$WINNING_POSITION))
 do
+	rollDieOutcome=$((RANDOM%6+1))
+	echo "Roll die outcome is:" $rollDieOutcome
 	optionCheck=$((RANDOM%3))
 	case $optionCheck in
 		$noPlay)
 			echo "Current position is:"$position
 			echo "You stay in same position:"$position
+			position=$position
+			printf "\n"
 			;;
 		$ladder)
-			((ladderCount++))
 			echo "Current position is :" $position
-			position=$((position+rollDieOutcome))
-			echo "You encountered a ladder. new position is:" $position
+			echo "You encountered a ladder."
+			# Exact winning condition
+			if(( $((position+rollDieOutcome))>100  ))
+			then
+				position=$position
+				echo "Current position is:"$position
+			else
+				position=$((position+rollDieOutcome))
+				echo "New position is:"$position
+			fi
+			printf "\n"
 			;;
 		$snake)
-			((snakeCount++))
+			echo "You Encountered snake."
 			echo "Current position is :" $position
-			position=$((position-rollDieOutcome))
+			# Exact restart condition
 			if(($position<0))
 			then
-				echo "You encountered a snake. new position is:" $POSITION
+				echo "New position is:" $POSITION
 				position=$POSITION
 			else
-				echo "You encountered a snake. new position is:" $position
+				position=$((position-rollDieOutcome))
+				echo "New position is:" $position
 				position=$position
 			fi
+			printf "\n"
 			;;
 	esac
 done
